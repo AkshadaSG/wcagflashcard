2cb2fa7a85c51b325b33ff451420a9b048847d8f
AkshadaShe <akshada.shelke@trafilea.com>
2023-04-21 17:26:27 +0530

    Added tabindex and onKeyPress functionality

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ import Data from "./data.json";
 
 const Card = ({ project }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+
   return (
     <>
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
         <div
           className={styles.cardOne_container}
           onClick={() => setIsFlipped((prev) => !prev)}
+          onKeyPress={() => setIsFlipped((prev) => !prev)}
+          tabindex="0"
+          role="img"
+          aria-label="Flashcard Image"
         >
-          <img className={styles.cardOne} src={project.src} alt={project.alt} />
+          <img className={styles.cardOne} src={project.src} alt={project.alt} title={project.description}/>
           <span className={styles.tooltiptext}> {project.alt}</span>
         </div>
-        <div
+        <h2
           className={styles.cardTwo}
           onClick={() => setIsFlipped((prev) => !prev)}
+          onKeyPress={() => setIsFlipped((prev) => !prev)}
         >
           {project.description}
-        </div>
+        </h2>
       </ReactCardFlip>
     </>
   );
@@ -30,10 +36,16 @@ const Projects = () => {
   return (
     <>
       <header role="banner" className={styles.banner}>
-        <h1 className={styles.banner_title}>Flashcards Interaction</h1>
+        <h1 className={styles.banner_title} tabindex="0">
+          Flashcards Interaction
+        </h1>
       </header>
       <main role="main" className={styles.container}>
-        <div className={styles.card_section}>
+        <div
+          role="navigation"
+          aria-label="Flash Cards"
+          className={styles.card_section}
+        >
           <div className={styles.cardList}>
             {Data.map((item, index) => (
               <Card project={item} key={`card-${index}`} />
